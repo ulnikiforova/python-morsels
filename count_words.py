3954c727797d741b9666fdc6a0064f3bc93bae59
ulnikiforova <ulnikiforova@gmail.com>
2024-10-23 12:33:19 +0200

    count_words solution

---
+++ count_words.py
@@ -0,0 +1,8 @@
+import re
+def count_words(str):
+    words = re.split(r'\W+', str) # only Unicode alphanumeric words + underscore
+    # words = str.split() # words separated by whitespaces only
+    words_count = dict.fromkeys(words,0)
+    for item in words:
+        words_count[item] += 1
+    return words_count
