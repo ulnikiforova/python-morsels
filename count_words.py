5badb5b9705a4f0ad23a37fb961b0a0423eb576b
ulnikiforova <ulnikiforova@gmail.com>
2024-10-23 12:39:01 +0200

    changed default solution: considers apostrophes as separators

--- count_words.py
+++ count_words.py
@@ -1,8 +1,10 @@
 import re
 def count_words(str):
-    words = re.split(r'\W+', str) # only Unicode alphanumeric words + underscore
-    # words = str.split() # words separated by whitespaces only
+    # words = re.split(r'\W+', str) # only Unicode alphanumeric words + underscore
+    words = str.split() # words separated by whitespaces only
     words_count = dict.fromkeys(words,0)
     for item in words:
         words_count[item] += 1
     return words_count
+
+print(count_words("don't stop believing"))
