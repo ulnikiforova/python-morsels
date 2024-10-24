5f1fbd908f3c757605deb8f8f29a8b81119b19c4
ulnikiforova <ulnikiforova@gmail.com>
2024-10-24 18:32:40 +0200

    healthy man tuple comparison

--- get_earliest.py
+++ get_earliest.py
@@ -1,20 +1,12 @@
 def compare_dates(date1_str, date2_str):
     m1, d1, y1 = tuple(list(date1_str.split('/')))
     m2, d2, y2 =  tuple(list(date2_str.split('/')))
-    if y1 < y2:
+    date1_ordered = (y1, m1, d1)
+    date2_ordered = (y2, m2, d2)
+    if date1_ordered <= date2_ordered:
         return date1_str
-    elif y1 > y2:
-        return date2_str
     else:
-        if m1 < m2:
-            return date1_str
-        elif m1 > m2:
-            return date2_str
-        else:
-            if d1 < d2:
-                return date1_str
-            else:
-                return date2_str
+        return date2_str
 
 def get_earliest(*dates):
     earliest_date = dates[0]
